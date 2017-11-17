2aefc3e10ca5f1dfd88799ac68e400a46d4f50cb
Roman Hronza <Roman Hronza@DESKTOP-36MHAVD>
2017-11-17 19:55:43 +0100

    commit

---
+++ src/main/java/cz/expertkom/ju/interfaces/UserService.java
@@ -0,0 +1,9 @@
+package cz.expertkom.ju.interfaces;
+
+import cz.expertkom.ju.interfaces.entity.User;
+
+public interface UserService {
+
+	User getUser(String username);
+	
+}
---
+++ src/main/java/cz/expertkom/ju/interfaces/entity/User.java
@@ -0,0 +1,49 @@
+package cz.expertkom.ju.interfaces.entity;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String username;
+
+	private String firstname;
+
+	private String lastname;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", firstname=" + firstname + ", lastname=" + lastname + "]";
+	}
+	
+	
+
+}
